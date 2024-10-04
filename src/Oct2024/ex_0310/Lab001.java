cfc3dd6cdf96346229dde3914ed2ecde882e783d
sindhunagai <snagai.b@gmail.com>
2024-10-04 19:49:23 +0800

    commit

--- src/Oct2024/ex_0310/Lab001.java
+++ src/Oct2024/ex_0310/Lab001.java
@@ -1,4 +1,8 @@
 package Oct2024.ex_0310;
 
 public class Lab001 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
 }
---
+++ src/Oct2024/ex_0410/Lab002.java
@@ -0,0 +1,8 @@
+package Oct2024.ex_0410;
+
+public class Lab002 {
+    public static void main(String[] args)
+    {
+        System.out.println("Times table of 10" );
+    }
+}
