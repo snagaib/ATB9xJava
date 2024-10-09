eee22ee08911d1cbf22938bedc56b5f73ed4dc0a
sindhunagai <snagai.b@gmail.com>
2024-10-09 22:24:18 +0800

    9-oct-printf

---
+++ src/Oct2024/ex_0910/Lab003.java
@@ -0,0 +1,22 @@
+package Oct2024.ex_0910;
+
+import java.io.PrintStream;
+
+public class Lab003 {
+    public static void main(String args[])
+    {
+        int tab=5;
+        System.out.printf("%d x 1 = %d", tab, tab * 1);
+        System.out.printf("\n");
+        System.out.printf("%d x 2 = %d", tab, tab * 2);
+        System.out.printf("\n");
+        System.out.printf("%d x 3 = %d", tab, tab * 3);
+        System.out.printf("\n");
+        System.out.printf("%d x 4 = %d", tab, tab *4 );
+        System.out.printf("\n");
+        System.out.printf("%d x 5 = %d", tab, tab * 5);
+        System.out.printf("\n");
+
+
+    }
+}
